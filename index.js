45ee2b4742f0a8886b33f2c7b62a482adbfeeb68
Aleksey <fduchunru@gmail.com>
2020-10-11 19:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your doToElementsInArray() function here:
-var evens = [0, 2, 4, 6, 8, 10];
+//var evens = [0, 2, 4, 6, 8, 10];
 
 function toDoElementsInArray(array, callback){
   array.forEach(callback)
