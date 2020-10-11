c34d9117280c848581d6eef262d66febb8fee225
Aleksey <fduchunru@gmail.com>
2020-10-11 19:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Add your doToElementsInArray() function here:
-function toDoElementsInArray(){
-  
+var evens = [0, 2, 4, 6, 8, 10];
+
+function toDoElementsInArray(array, callback){
+  array.forEach(callback)
 }
 // Add your changeCompletely() function here:
