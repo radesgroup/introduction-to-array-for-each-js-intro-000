a822d3c3cdd481d7391fdf053a98dea071909905
Aleksey <fduchunru@gmail.com>
2020-10-11 20:17:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
+//var evens = [0, 2, 4, 6, 8, 10];
 
-
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
+}
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
